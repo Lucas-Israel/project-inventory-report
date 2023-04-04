70a7639ac01c88a323ac5dda5ad7ee00abd06dbf
Lucas Israel <lisraelalves92@gmail.com>
2023-04-04 17:56:28 -0300

    feat: :sparkles: methods visibility changed, complete_report done for req 3
    simple_report static methods changed from private to protected

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,136 @@
-class CompleteReport:
-    pass
+if __name__ == "__main__":
+    from simple_report import SimpleReport
+else:
+    from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(list):
+        oldest_product = CompleteReport._get_oldest_product(list)
+        nearest_expiration = CompleteReport._get_nearest_expiration_date(list)
+        most_products = CompleteReport._get_company_with_more_products(list)
+        first_message = CompleteReport._generate_message(
+            oldest_product, nearest_expiration, most_products
+        )
+        company_inventory_dict = CompleteReport._get_company_max_inventory(
+            list
+        )
+        second_message = CompleteReport._generate_stock_message(
+            company_inventory_dict
+        )
+        return CompleteReport._generate_complete_message(
+            first_message, second_message
+        )
+
+    @staticmethod
+    def _get_company_max_inventory(list):
+        new_dict = {}
+        for product in list:
+            if product.get("nome_da_empresa") in new_dict:
+                new_dict[product.get("nome_da_empresa")] += 1
+            else:
+                new_dict[product.get("nome_da_empresa")] = 1
+        return new_dict
+
+    @staticmethod
+    def _generate_stock_message(dict):
+        message = "Produtos estocados por empresa:\n"
+
+        for element in dict:
+            message += f"- {element}: {dict[element]}\n"
+
+        return message
+
+    @staticmethod
+    def _generate_complete_message(message1, message2):
+        return f"{message1}\n{message2}"
+
+
+if __name__ == "__main__":
+    a = [
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "Forces of Nature",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "Forces of Nature",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "Forces of Nature",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "aaa",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "aaa",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "zzz",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "zzz",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "zzz",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "MESA",
+            "nome_da_empresa": "zzz",
+            "data_de_fabricacao": "2022-05-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+        },
+    ]
+
+    b = CompleteReport.generate(a)
+    print(b)
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,15 +4,15 @@ from datetime import datetime
 class SimpleReport:
     @staticmethod
     def generate(list):
-        oldest_product = SimpleReport.__get_oldest_product(list)
-        nearest_expiration = SimpleReport.__get_nearest_expiration_date(list)
-        most_products = SimpleReport.__get_company_with_more_products(list)
-        return SimpleReport.__generate_message(
+        oldest_product = SimpleReport._get_oldest_product(list)
+        nearest_expiration = SimpleReport._get_nearest_expiration_date(list)
+        most_products = SimpleReport._get_company_with_more_products(list)
+        return SimpleReport._generate_message(
             oldest_product, nearest_expiration, most_products
         )
 
     @staticmethod
-    def __get_oldest_product(list):
+    def _get_oldest_product(list):
         oldest = ""
         today = datetime.today().strftime("%Y%m%d")
         diff_check = 0
@@ -25,7 +25,7 @@ class SimpleReport:
         return oldest
 
     @staticmethod
-    def __get_nearest_expiration_date(list):
+    def _get_nearest_expiration_date(list):
         newest = ""
         today = datetime.today().strftime("%Y%m%d")
         # diff_check needs to be one big number that is used for initial
@@ -40,7 +40,7 @@ class SimpleReport:
         return newest
 
     @staticmethod
-    def __get_company_with_more_products(list):
+    def _get_company_with_more_products(list):
         new_list = []
 
         for product in list:
@@ -49,9 +49,71 @@ class SimpleReport:
         return max(set(new_list), key=new_list.count)
 
     @staticmethod
-    def __generate_message(old_prod, nearest_expiration, most_prod):
+    def _generate_message(old_prod, nearest_expiration, most_prod):
         return (
             f"Data de fabricação mais antiga: {old_prod}\n"
             f"Data de validade mais próxima: {nearest_expiration}\n"
             f"Empresa com mais produtos: {most_prod}"
         )
+
+
+if __name__ == "__main__":
+    to_send = [
+        {
+            "id": 1,
+            "nome_do_produto": "XABLAU",
+            "nome_da_empresa": "Forces of Nature",
+            "data_de_fabricacao": "2022-04-05",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar em local fresco",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "CADEIRA",
+            "nome_da_empresa": "Forces of Nature",
+            "data_de_fabricacao": "2022-04-03",
+            "data_de_validade": "2023-03-03",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar em local fresco",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "XABLAU",
+            "nome_da_empresa": "zzz",
+            "data_de_fabricacao": "2008-04-05",
+            "data_de_validade": "2023-04-05",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar em local fresco",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "XABLAU",
+            "nome_da_empresa": "aaa",
+            "data_de_fabricacao": "2008-04-05",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar em local fresco",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "XABLAU",
+            "nome_da_empresa": "aaa",
+            "data_de_fabricacao": "2008-04-05",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar em local fresco",
+        },
+        {
+            "id": 1,
+            "nome_do_produto": "XABLAU",
+            "nome_da_empresa": "aaa",
+            "data_de_fabricacao": "2008-04-05",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "Conservar em local fresco",
+        },
+    ]
+
+    a = SimpleReport.generate(to_send)
+    print(a)
