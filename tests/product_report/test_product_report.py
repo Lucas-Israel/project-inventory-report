4357b8626750fe0152014d1ed3c55d630e539301
Lucas Israel <lisraelalves92@gmail.com>
2023-04-05 12:13:14 -0300

    test: :white_check_mark: added test for req 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+expected = (
+    "O produto abc fabricado em 2000-01-01 por def com validade até "
+    "2001-01-01 precisa ser armazenado na luz."
+)
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    a = Product(
+        id=1,
+        nome_do_produto="abc",
+        nome_da_empresa="def",
+        data_de_fabricacao="2000-01-01",
+        data_de_validade="2001-01-01",
+        numero_de_serie=123456789,
+        instrucoes_de_armazenamento="na luz",
+    )
+    assert repr(a) == expected
